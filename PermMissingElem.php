d7682659cd3359198d6bf2bffa4791abc48bb840
Kavinda Lakmal Bandara <kavindalb@gmail.com>
2019-03-17 21:23:15 +0530

    Added solution 3

--- PermMissingElem.php
+++ PermMissingElem.php
@@ -2,8 +2,22 @@
 echo solution([2,3,4,1]);
 function solution($A){
 	/*
-	 * Solution 2: Correctness 100%; performance 100%
+	 * Solution 3: Correctness 100%; performance 100%
 	 */
+    $arrayCount = count($A);
+    $lastNumber = $arrayCount+1;
+    $firstNumber = 1;
+    
+    //Sum of given array
+    $sumA = array_sum($A);
+    // Sum of arithmetic series
+    $sumOfSeries = (($arrayCount+1)*($firstNumber + $lastNumber)) / 2;
+
+    return (int)($sumOfSeries - $sumA);
+
+	/*
+	 * Solution 2: Correctness 100%; performance 100%
+	 *
 	sort($A);
 	for($i=0;$i<count($A); $i++){
 		if($A[$i]!=$i+1){
